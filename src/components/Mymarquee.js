393b251642226437db13d975af41276e7f51591b
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-05-09 08:42:24 +0700

    Revert changes made by npm audit fix --force

---
+++ src/components/Mymarquee.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Marquee from "react-fast-marquee";
+
+const Mymarquee = () => {
+  return (
+    <div>
+      <Marquee>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+      </Marquee>
+    </div>
+  );
+};
+
+export default Mymarquee;
